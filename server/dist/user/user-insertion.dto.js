82c998bcb6ff47ec45e18daf31d393196b467923
DavidSantander <david.santander@comunidad.unam.mx>
2020-07-05 00:00:02 -0500

    Remove dist in git

--- server/dist/user/user-insertion.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserInsertionDto = void 0;
-class UserInsertionDto {
-}
-exports.UserInsertionDto = UserInsertionDto;
-//# sourceMappingURL=user-insertion.dto.js.map
